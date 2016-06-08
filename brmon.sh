0da34c8ce4a823eeed7fa173c2a5c7d95c4ea2e3
OfotViking <ofotviking@gmail.com>
2016-06-08 13:23:22 +0200

    update

--- brmon.sh
+++ brmon.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-grep1='EU|UK|DE|FR|US'
-grep2='Hardcore|Regular'
-#grep2='Hardcore|Squads|WAKE|Stratis\|Hardcore'
+# exclude US, show only Altis/bozcaada SOLO 
+grepx='US'
+grep1='Altis\|HC|Bozcaada\|HC'
+grep2='SOLO'
 
 while true; do
 # loop start
 
-grab=$(wget -qO - http://battleroyale.gamingdeluxe.co.uk/status/)
-servers=$(echo "$grab" | grep server_name | grep -v OFFLINE | egrep $grep1 | egrep $grep2 | sed 's/  /_/g' | sed 's/ /_/g' | sed 's/>/> /g' | sed -e 's/<[^>]*>//g' | sed 's/&nbsp;//g' | sed 's/   / /g' | sed 's/  / /g' | sed 's/ //' | sed 's/_ /_/' | rev | cut -d" " -f 3-10 | rev)
+grab=$(wget -qO - http://status.battleroyalegames.com/)
+servers=$(echo "$grab" | grep server_name | grep -v OFFLINE | egrep -v $grepx | egrep $grep1 | egrep $grep2 | sed 's/  /_/g' | sed 's/ /_/g' | sed 's/>/> /g' | sed -e 's/<[^>]*>//g' | sed 's/&nbsp;//g' | sed 's/_Battle_Royale_._battleroyalegames.com//g' | sed 's/   / /g' | sed 's/  / /g' | sed 's/ //' )
 
-refreshtime=$(echo "$grab" | grep JavaScript:timedRefresh | cut -d"\"" -f 2 | cut -d"(" -f2 | cut -d"\"" -f 2 | cut -d")" -f1)
-refreshseconds=$(expr $refreshtime / 1000 + 30)
+refreshtime=$(echo "$grab" | grep JavaScript:timedRefresh | cut -d"\"" -f 2 | cut -d"(" -f2 | cut -d"\"" -f 2 | cut -d")" -f1) # use the same refreshinterval as on website
+refreshseconds=$(expr $refreshtime / 1000 + 60) # add 60 seconds if all servers empty
 
 clear
 
-echo "Arma 3 Battle Royale server status"
+echo "Arma 3 Battle Royale server status/notifier/logger"
 echo " "
 
 while read -r line; do
-  name=$(echo -n "$line" | cut -d" " -f1,2 | cut -d"_" -f1)
-  id=$(echo -n "$line" | cut -d" " -f1,2 | md5sum | cut -d " " -f1)
-  ip=$(echo -n "$line" | cut -d" " -f4)
-  status=$(echo -n "$line" | cut -d" " -f3)
-  players=$(echo -n "$line" | cut -d" " -f5 | cut -d"/" -f1)
+  name=$(echo -n "$line" | cut -d" " -f1,2,3 | cut -d"_" -f1 | sed 's/ //g')
+  id=$(echo -n "$line" | cut -d" " -f1,2,3 | md5sum | cut -d " " -f1)
+  ip=$(echo -n "$line" | cut -d" " -f5)
+  status=$(echo -n "$line" | cut -d" " -f4)
+  players=$(echo -n "$line" | cut -d" " -f6 | cut -d"/" -f1)
   date=$(cat /tmp/date$id)
-  echo -n $name \| $status \| $date \| $players \| $ip \|
+  
+  if [ $status = SERVER_OPEN ] && (( $players > 1 )); then
+  status_color="\e[32m$status\e[0m"
+  else
+  status_color="$status"
+  fi
+  
+  date2=$(echo $date | cut -d" " -f2-4)
+  
+  printf "%-27s %s" " $name" \|
+  printf "%-17s %s" " $status_color" \|
+  printf "%-15s %s" " $date2" \|
+  printf "%-3s %s" " $players" \|
+  printf "%-21s %s" " $ip" \|
   
   touch /tmp/notified$id
   touch /tmp/date$id
@@ -34,12 +48,14 @@ while read -r line; do
   if [ $status = SERVER_OPEN ] && [ "$(cat /tmp/notified$id)" != "yes" ] && (( $players > 16 )); then
   echo -n  " New server filling up, notification sendt"
   echo -n $'\a' #beep
+  refreshseconds=$(expr $refreshtime / 1000)
   #notify-command here
-  #./GrowlNotify --finished "$name"
+  ./GrowlNotify --finished "$name"
   echo -n "yes" > /tmp/notified"$id"
   fi
   if [ $status = SERVER_OPEN ] && [ "$(cat /tmp/notified$id)" = "yes" ] && (( $players > 17 )); then
   echo -n  " Server filling up, join NOW!"
+  refreshseconds=$(expr $refreshtime / 1000)
   #echo $'\a' #beep
   fi
   if [ $status = GAME_IN_PROGRESS ] && [ "$(cat /tmp/notified$id)" != "no" ]; then
@@ -52,7 +68,7 @@ while read -r line; do
   #date +%Y%m%d-%H%M%S
   date=$(cat /tmp/date$id)
   echo -n "no" > /tmp/logged"$id"
-  refreshseconds=30
+  refreshseconds=$(expr $refreshtime / 1000)
   fi
   if [ $status != GAME_IN_PROGRESS ] && [ "$(cat /tmp/logged$id)" != "yes" ]; then
   echo $name \| $date >> log.txt
